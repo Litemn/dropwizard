efd00e468d9a2007493d548cb9304b71ddcbe4da
Artem Prigoda <arteamon@gmail.com>
2018-02-26 11:32:42 +0100

    Add tests for ResilentUdpSocketOutputStream

--- dropwizard-logging/src/main/java/ch/qos/logback/core/recovery/ResilentUdpSocketOutputStream.java
+++ dropwizard-logging/src/main/java/ch/qos/logback/core/recovery/ResilentUdpSocketOutputStream.java
@@ -6,6 +6,11 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Represents a resilent UDP connection as an {@link OutputStream}. If the client can't send
+ * data to the server, it automatically tries to reconnect to it. Of course UDP is not
+ * reliable by definition, by this allows to us to handle sporadic network hiccups and DNS issues.
+ */
 public class ResilentUdpSocketOutputStream extends ResilientOutputStreamBase {
 
     private final String host;
@@ -15,7 +20,7 @@ public class ResilentUdpSocketOutputStream extends ResilientOutputStreamBase {
         this.host = host;
         this.port = port;
         try {
-            super.os = openNewOutputStream();
+            this.os = openNewOutputStream();
         } catch (IOException e) {
             throw new IllegalStateException("Unable to create an UDP connection to " + host + ":" + port, e);
         }
